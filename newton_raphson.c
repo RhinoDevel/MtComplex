1a10cc28eb6bc7fcf1b349e1b9f25a2446b54561
Marc <RhinoDevel@users.noreply.github.com>
2025-09-15 16:27:44 +0200

    Add newton_raphson.

---
+++ newton_raphson.c
@@ -0,0 +1,30 @@
+
+// RhinoDevel, Marcel Timm, 2025sep15
+
+#include "newton_raphson.h"
+
+#include <math.h> // For NAN.
+#include <assert.h>
+
+double newton_raphson(
+    int const max_steps,
+    double const done_diff,
+    double const initial_guess,
+    double const val,
+    double (*step_func)(double, double))
+{
+    double x_last = 0.0;
+    double x = initial_guess;
+
+    for(int n = 1; n <= max_steps; ++n)
+    {
+        x_last = x;
+        x = step_func(x, val);
+
+        if(fabs(x - x_last) < done_diff)
+        {
+            return x;
+        }
+    }
+    return NAN;
+}
---
+++ newton_raphson.h
@@ -0,0 +1,25 @@
+
+// RhinoDevel, Marcel Timm, 2025sep15
+
+#ifndef MT_NEWTON_RAPHSON
+#define MT_NEWTON_RAPHSON
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * - Returns NAN, if no reasonably precise result was found by approximation.
+ */
+double newton_raphson(
+    int const max_steps,
+    double const done_diff,
+    double const initial_guess,
+    double const val,
+    double (*step_func)(double, double));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MT_NEWTON_RAPHSON

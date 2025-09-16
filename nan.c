20c65a02f10ad737c86d9661be18acd014fe36fa
Marc <RhinoDevel@users.noreply.github.com>
2025-09-16 11:14:57 +0200

    nan (unused).

---
+++ nan.c
@@ -0,0 +1,28 @@
+
+// RhinoDevel, Marcel Timm, 2025sep16
+
+#include "nan.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+bool nan_is(double const val)
+{
+    return val != val;
+}
+
+double nan_get(void)
+{
+    // Modern compilers should reduce this:
+
+    static uint64_t const nan_int = 0x7ff8000000000000ULL;
+    
+    double ret_val = 0.0;
+
+    memcpy(&ret_val, &nan_int, sizeof ret_val);
+
+    assert(nan_is(ret_val));
+
+    return ret_val;
+}
---
+++ nan.h
@@ -0,0 +1,21 @@
+
+// RhinoDevel, Marcel Timm, 2025sep16
+
+#ifndef MT_NAN
+#define MT_NAN
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool nan_is(double const);
+
+double nan_get(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MT_NAN

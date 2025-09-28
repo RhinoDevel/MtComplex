25f2c5430d910a818f590fc0b299c913f436b1c5
Marc <RhinoDevel@users.noreply.github.com>
2025-09-28 17:40:53 +0200

    znr_pol: Replace all math.h stuff!

--- baremath/znr_pol.c
+++ baremath/znr_pol.c
@@ -2,10 +2,9 @@
 // RhinoDevel, Marcel Timm, 2025sep16
 
 #include "znr_pol.h"
+#include "atan.h"
 #include "znr.h"
 #include "ln.h"
-
-#include <math.h> // TODO: Replace with own code!
 
 double znr_pol_magnitude(struct znr const nr)
 {
@@ -14,10 +13,17 @@ double znr_pol_magnitude(struct znr const nr)
 
 double znr_pol_phi(struct znr const nr)
 {
-    return atan2(nr.i, nr.r);
+    return atan_atan2(nr.i, nr.r);
 }
 
 struct znr znr_pol_from(double const phi, double const magnitude)
 {
-    return (struct znr){ .r = magnitude * cos(phi), .i = magnitude * sin(phi) };
+    // The terms to calculate cosine & sine.
+    static int const n_terms = 50; // TODO: More or less?
+
+    struct znr const phi_complex = (struct znr){ .r = phi, .i = 0.0 };
+    double const cos_phi = znr_cos(phi_complex, n_terms).r;
+    double const sin_phi = znr_sin(phi_complex, n_terms).r;
+
+    return (struct znr){ .r = magnitude * cos_phi, .i = magnitude * sin_phi };
 }

a1106d8849fca5b7f78d24eb6c7d1e3eb9a14ee8
Marc <RhinoDevel@users.noreply.github.com>
2025-09-29 10:49:35 +0200

    Add deb.h.

---
+++ baremath/deb.h
@@ -0,0 +1,43 @@
+
+// Marcel Timm, RhinoDevel, 2016feb29
+
+#ifndef MT_DEB
+#define MT_DEB
+
+#include <stdio.h>
+
+#ifdef NDEBUG
+#define MT_DEB_ACTIVE 0
+#else //NDEBUG
+#define MT_DEB_ACTIVE 1
+#endif //NDEBUG
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * - Original source code:
+ * 
+ *   http://stackoverflow.com/questions/1644868/c-define-macro-for-debug-printing
+ */
+#define deb_line(fmt, ...) \
+    do \
+    { \
+        if(MT_DEB_ACTIVE) \
+        { \
+            fprintf( \
+                stderr, \
+                "DEBUG: %s, %d, %s() : " fmt "\n", \
+                __FILE__, \
+                __LINE__, \
+                __func__, \
+                ##__VA_ARGS__); \
+        } \
+    }while(0);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //MT_DEB
